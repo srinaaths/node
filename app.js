cbd4f15d07b5e479a3491a22268c5856e702a4a2
srinaaths <srinaath.s@surveysparrow.com>
2022-06-27 14:37:50 +0530

    check
    check, test

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-	console.log('req incoming')
-	res.statusCode = 200
+	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain')
-	res.end('Hello World');
+	res.end('hello')
 })
 
-server.listen(8081, () => console.log('server running'))
+server.listen(8081, () => console.log('hello'));
