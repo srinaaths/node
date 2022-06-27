1ad4e63517eeb61a492843e5493935fe56a48652
srinaaths <srinaath.s@surveysparrow.com>
2022-06-27 14:45:29 +0530

    updating checking changes

--- app.js
+++ app.js
@@ -4,6 +4,4 @@ const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain')
 	res.end('hello')
-})
-
-server.listen(8081, () => console.log('listening to port 8081'));
+}).listen(8081, () => console.log('listening to port 8081'));
