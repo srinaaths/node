1bef8939d790c290b1337113536a5b79ff4cca0e
srinaaths <srinaath.s@surveysparrow.com>
2022-06-27 15:31:45 +0530

    comments added

--- events.js
+++ events.js
@@ -10,3 +10,16 @@ eventEmitter.emit('hi')
 eventEmitter.on('name', name => console.log(name))
 
 eventEmitter.emit('name', 'sri')
+
+/*
+EventEmitter - core module of node
+
+EventEmitter is a class - container for properties and methods
+
+create an object for this class
+
+emit - passing a signal - raised an event
+
+listener - needed to listen to the event - on - name of the event and the callback function
+
+*/
