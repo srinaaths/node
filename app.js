bece1f5653cbcd6db5827e619eb8f6fd7f5905e1
Srinaath S <srinaath.s@gmail.com>
2022-06-27 12:51:44 +0530

    http server

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const http = require('http')
 
+http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('hello')
+}).listen(8081, () => console.log('listening to port 8081'));
